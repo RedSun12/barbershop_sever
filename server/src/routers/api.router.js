aa9bae866a48e46119fd4c57e9847ef420351c67
RedSun12 <wakeupup@yandex.ru>
2024-07-30 20:34:28 +0300

    new

--- server/src/routers/api.router.js
+++ server/src/routers/api.router.js
@@ -4,6 +4,7 @@ const authRouter = require('./auth.api.router');
 const tokenRouter = require('./token.api.router');
 const productRouter = require('./product.api.router')
 
+
 router.use('/tokens', tokenRouter);
 router.use('/auth', authRouter);
 router.use('/', gamesRouter);
