b088cf8b1e1c152a0d6c561cba4c6a1045622f58
RedSun12 <wakeupup@yandex.ru>
2024-07-30 21:33:12 +0300

    newAdd

--- server/src/routers/api.router.js
+++ server/src/routers/api.router.js
@@ -5,7 +5,7 @@ const tokenRouter = require('./token.api.router');
 const scoreRouter = require('./score.api.router')
 const fotoRouter = require('./barberfoto.api.router')
 
-router.use('/tokens', tokenRouter);
+router.use('/tokens', tokenRouter); 
 router.use('/auth', authRouter);
 router.use('/', gamesRouter);
 router.use('/user', scoreRouter);
