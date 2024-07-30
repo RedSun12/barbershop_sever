72e2fe8c2a06f858080a75d811c9606c8455dd3a
RedSun12 <wakeupup@yandex.ru>
2024-07-30 21:07:15 +0300

    product_card

---
+++ client/src/pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,12 @@
+import List from '../../components/List/List';
+import styles from './ProductPage.module.css';
+import Form from '../../components/Form/Form';
+
+export default function ProductPage() {
+  return (
+    <div className={styles.wrapper}>
+      <Form />
+      <List />
+    </div>
+  );
+}
