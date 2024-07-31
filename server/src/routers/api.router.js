4985c857af9f947d097f6f625b8a8b95c728e664
EdKhismatov <E.Khismatov@yandex.ru>
2024-07-31 17:01:52 +0200

    services

--- server/src/routers/api.router.js
+++ server/src/routers/api.router.js
@@ -4,11 +4,13 @@ const authRouter = require('./auth.api.router');
 const tokenRouter = require('./token.api.router');
 const fotoRouter = require('./barberfoto.api.router')
 const productRouter = require('./product.api.router')
+const serviceRouter = require('./service.api.router')
 
 router.use('/tokens', tokenRouter); 
 router.use('/auth', authRouter);
 router.use('/', gamesRouter);
 router.use('/foto', fotoRouter);
 router.use('/product', productRouter);
+router.use('/service', serviceRouter);
 
 module.exports = router;
