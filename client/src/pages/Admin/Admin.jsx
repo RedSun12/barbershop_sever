dee42edbf2d347269ae08202fb150dd409933b2d
EdKhismatov <E.Khismatov@yandex.ru>
2024-08-01 13:05:24 +0200

    slider

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,12 +1,15 @@
 import styles from './Admin.module.css';
 import FormServicesUpdate from '../../components/ComponentsAdmin/FormServicesUpdate/FormServicesUpdate';
 import BarberspopAdressAdmin from '../../components/ComponentsAdmin/BarberspopAdressAdmin/BarberspopAdressAdmin'
+import SliderAdmin from '../../components/ComponentsAdmin/SliderAdmin/SliderAdmin';
+
 
 export default function Admin() {
   return (
     <div className={styles.admin}>
       <FormServicesUpdate />
       <BarberspopAdressAdmin/>
+      <SliderAdmin/>
     </div>
   )
 }
