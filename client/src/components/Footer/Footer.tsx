8ac435eaeed0b7348e1eb65b2eb8adabc74ffa7c
EdKhismatov <E.Khismatov@yandex.ru>
2024-08-08 20:11:44 +0200

    styles

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,56 +5,84 @@ import axiosInstance from '../../axiosInstance';
 const { VITE_API } = import.meta.env;
 
 export default function Footer() {
-    const [contacts, setContacts] = useState({
-        adress: '',
-        telephone: '',
-      });
-    
-      useEffect(() => {
-        (async function () {
-          const { data } = await axiosInstance.get(`${VITE_API}/foto/contact/`);
-          setContacts(data[0]);
-        })();
-      }, []);
+  const [contacts, setContacts] = useState({
+    adress: '',
+    telephone: '',
+  });
 
-      function loadScript() {
-        var script = document.createElement('script');
-        script.src = 'https://w1168615.yclients.com/widgetJS';
-        script.charset = 'UTF-8';
-        document.body.appendChild(script);
-      } 
+  useEffect(() => {
+    (async function () {
+      const { data } = await axiosInstance.get(`${VITE_API}/foto/contact/`);
+      setContacts(data[0]);
+    })();
+  }, []);
 
-      useEffect(() => {
-        loadScript();
-      }, []);
+  function loadScript() {
+    var script = document.createElement('script');
+    script.src = 'https://w1168615.yclients.com/widgetJS';
+    script.charset = 'UTF-8';
+    document.body.appendChild(script);
+  }
 
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.container}>
-                <div className={styles.logo}>
-                    <img className={styles.image} src={'/logoFooter2.png'} alt="Логотип Север" />
-                    <div className={styles.name}>БАРБЕРШОП СЕВЕР</div>
-                </div>
-                <div className={styles.contact}> 
-                    <nav className={styles.navigation}>
-                        <ul>
-                            <li><Link to='/'><strong>Главная</strong></Link></li>
-                            <li><Link to='/services'>Услуги</Link></li>
-                            <li ><Link to='/product'>Товары</Link></li>
-                            <li><Link to='/contact'>Контакты</Link></li>
-                            <a href="w1168615.yclients.com/widgetJS" className="ms_booking" onClick={loadScript()}>Записаться</a>
-                        </ul>
-                    </nav>
-                    <div className={styles.info}>
-                        <ul>
-                            <li className={styles.item}><strong>Контакты</strong></li>
-                            <li className={styles.item}>barbershopsever@mail.ru</li>
-                            <li className={styles.item}>{contacts.telephone}</li>
-                            <li className={styles.item}><Link to='/instagram'>наш instagram</Link></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  useEffect(() => {
+    loadScript();
+  }, []);
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <div className={styles.loader}>
+            <img src="/load1.png" alt="Loading" className={styles.spining} />
+            <img
+              src="/load2.png"
+              alt="Loading"
+              
+              className={`${styles.loadingImage} ${styles.spinning}`}
+            />
+          </div>
+          <div className={styles.name}>БАРБЕРШОП СЕВЕР</div>
+        </div>
+        <div className={styles.contact}>
+          <nav className={styles.navigation}>
+            <ul>
+              <li>
+                <Link to="/">
+                  <strong>Главная</strong>
+                </Link>
+              </li>
+              <li>
+                <Link to="/services">Услуги</Link>
+              </li>
+              <li>
+                <Link to="/product">Товары</Link>
+              </li>
+              <li>
+                <Link to="/contact">Контакты</Link>
+              </li>
+              <a
+                href="w1168615.yclients.com/widgetJS"
+                className="ms_booking"
+                onClick={loadScript()}
+              >
+                Записаться
+              </a>
+            </ul>
+          </nav>
+          <div className={styles.info}>
+            <ul>
+              <li className={styles.item}>
+                <strong>Контакты</strong>
+              </li>
+              <li className={styles.item}>barbershopsever@mail.ru</li>
+              <li className={styles.item}>{contacts.telephone}</li>
+              <li className={styles.item}>
+                <Link to="/instagram">наш instagram</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
